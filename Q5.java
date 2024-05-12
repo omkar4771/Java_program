e68969c40421cec56a7c3012d734190c9ae402c4
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-12 10:54:59 +0530

    program 5

---
+++ Q5.java
@@ -0,0 +1,15 @@
+//Write a program to find the area and perimeter of a square?
+package program_200;
+public class Q5 
+{
+	public static void main(String[] args)
+	{
+		int a=50,area,perimeter;
+		area=a*a;
+		perimeter=4*a;
+		System.out.println("Area ="+area);
+		System.out.println("perimeter ="+perimeter);
+		
+	}
+
+}
