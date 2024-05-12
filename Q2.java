30a77ed75b2f1bf5b1ef54a3f0875f0830727f02
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-12 10:54:31 +0530

    program 2

---
+++ Q2.java
@@ -0,0 +1,16 @@
+/*Write a program to multiply and divide two numbers and print them
+ * in the form of equation (4*3=12 8/4=2)*/
+package program_200;
+public class Q2 
+{
+	public static void main(String[] args)
+	{
+		int a=42,b=12,multi,div;
+		multi=a*b;
+		div=a/b;
+		System.out.println(a+"*"+b+"="+multi);
+		System.out.println(a+"/"+b+"="+div);
+		
+	}
+
+}
