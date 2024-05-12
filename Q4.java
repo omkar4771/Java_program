e484ef30b12936abd70d12a3d0dfb5bb0124da01
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-12 10:54:48 +0530

    program 4

---
+++ Q4.java
@@ -0,0 +1,13 @@
+//Write a program to find the square root of a given number (use sqrt () function)?
+package program_200;
+import java.lang.Math;
+public class Q4 
+{
+	public static void main(String[] args) 
+	{
+		int a=50;
+		//Math.sqrt(a);
+		System.out.println(Math.sqrt(a));
+	}
+
+}
