e00e77504cbce9a96a3e4718413eebecad87a2aa
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-12 10:55:08 +0530

    program 6

---
+++ Q6.java
@@ -0,0 +1,16 @@
+//Write a program to find the area and circumference of a circle?
+package program_200;
+public class Q6 
+{
+	public static void main(String[] args) 
+	{
+		int r=45;
+		double A,C;
+		A=3.14*r*r;
+		C=2*3.14*r;
+		System.out.println("Area ="+A);
+		System.out.println("circumference ="+C);
+		
+	}
+
+}
