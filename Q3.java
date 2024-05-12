64ca4fd00c37a98cfd28d79b94369719f9ece432
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-12 10:54:40 +0530

    program 3

---
+++ Q3.java
@@ -0,0 +1,16 @@
+//Write a program to find the square and cube of a given number?
+package program_200;
+public class Q3 
+{
+
+	public static void main(String[] args)
+	{
+		int a=10,square,cube;
+		square=a*a;
+		cube=a*a*a;
+		System.out.println("square ="+square);
+		System.out.print("Cube ="+cube);
+
+	}
+
+}
